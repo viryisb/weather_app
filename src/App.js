e78f4a1388230fefc6a38b7691f3dac481d73c2a
Virginia <vbernasconi@altimetrik.com>
2022-08-28 17:29:57 -0300

    create form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./assets/css/App.css";
-import NavBar from "./components/NavBar";
+import './assets/css/App.css';
+import NavBar from './components/NavBar';
+import Form from './components/Form';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Form />
     </div>
   );
 }
